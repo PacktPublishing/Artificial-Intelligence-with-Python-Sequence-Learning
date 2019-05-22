203c440e2689f156b06a31181ecf6a5854aa7005
sonali-Packt <sonalis@packtpub.com>
2019-05-22 16:55:12 +0530

    Code Uploaded

---
+++ Section 2/stock_market.py
@@ -0,0 +1,51 @@
+import datetime
+import warnings
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.finance import quotes_historical_yahoo_ochl as quotes_yahoo
+from hmmlearn.hmm import GaussianHMM
+import pandas_datareader as pdr
+from datetime import datetime
+
+warnings.simplefilter("ignore", DeprecationWarning)
+
+stock_quotes = np.array(pdr.get_data_yahoo(symbols='INTC', start=datetime(1970, 9, 4), end=datetime(2016, 5, 17)))
+
+# Extract the closing quotes everyday
+closing_quotes = np.array([quote[2] for quote in stock_quotes])
+
+# Extract the volume of shares traded everyday
+volumes = np.array([quote[5] for quote in stock_quotes])[1:]
+
+# Take the percentage difference of closing stock prices
+diff_percentages = 100.0 * np.diff(closing_quotes) / closing_quotes[:-1]
+
+# Take the list of dates starting from the second value
+dates = np.array([quote[0] for quote in stock_quotes], dtype=np.int)[1:]
+
+# Stack the differences and volume values column-wise for training
+training_data = np.column_stack([diff_percentages, volumes])
+
+# Create and train Gaussian HMM
+hmm = GaussianHMM(n_components=7, covariance_type='diag', n_iter=1000)
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore')
+    hmm.fit(training_data)
+
+# Generate data using the HMM model
+num_samples = 300
+samples, _ = hmm.sample(num_samples)
+
+# Plot the difference percentages
+plt.figure()
+plt.title('Difference percentages')
+plt.plot(np.arange(num_samples), samples[:, 0], c='black')
+
+# Plot the volume of shares traded
+plt.figure()
+plt.title('Volume of shares')
+plt.plot(np.arange(num_samples), samples[:, 1], c='black')
+plt.ylim(ymin=0)
+
+plt.show()
